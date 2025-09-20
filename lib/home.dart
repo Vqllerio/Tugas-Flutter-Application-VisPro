9766b53d4630a3f4496bfc782892b36189085072
Vallerio Rayford Phoatmojo <valleriorayford36@gmail.com>
2025-09-21 00:04:58 +0800

    MDC-104

--- lib/home.dart
+++ lib/home.dart
@@ -13,61 +13,21 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'supplemental/asymmetric_view.dart';
 import 'model/product.dart';
 import 'model/products_repository.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-// TODO: Make a collection of cards (102)
-
-// Replace this entire method
   // TODO: Add a variable for Category (104)
+  final Category category;
+
+  const HomePage({this.category = Category.all, Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
-    return Scaffold(
-      // TODO: Add app bar (102)
-      appBar: AppBar(
-          // TODO: Add buttons and title (102)
-          leading: IconButton(
-            icon: const Icon(
-              Icons.menu,
-              semanticLabel: 'menu',
-            ),
-            onPressed: () {
-              print('Menu button');
-            },
-          ),
-          title: const Text('SHRINE'),
-          // TODO: Add trailing buttons (102)
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(
-                Icons.search,
-                semanticLabel: 'search',
-              ),
-              onPressed: () {
-                print('Search button');
-              },
-            ),
-            IconButton(
-              icon: const Icon(
-                Icons.tune,
-                semanticLabel: 'filter',
-              ),
-              onPressed: () {
-                print('Filter button');
-              },
-            ),
-          ]),
-      // TODO: Add a grid view (102)
-      body: AsymmetricView(
-        products: ProductsRepository.loadProducts(Category.all),
-      ),
+    return AsymmetricView(
+      products: ProductsRepository.loadProducts(category),
     );
   }
 }
