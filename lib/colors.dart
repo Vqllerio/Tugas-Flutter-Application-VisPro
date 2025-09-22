3b50ea03c89efdf22b7b72590b35ef6e9fc8524b
Vallerio Rayford Phoatmojo <valleriorayford36@gmail.com>
2025-09-22 08:23:42 +0800

    customization 1.1

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
-const kShrinePink50 = Color(0xFFFEEAE6);
-const kShrinePink100 = Color(0xFFFEDBD0);
-const kShrinePink300 = Color(0xFFFBB8AC);
-const kShrinePink400 = Color(0xFFEAA4A4);
+const kShrinePink50 = Color(0xFFE0F7FA); // Light teal
+const kShrinePink100 = Color(0xFFB2EBF2); // Soft aqua
+const kShrinePink300 = Color(0xFF4DD0E1); // Medium teal
+const kShrinePink400 = Color(0xFF26C6DA); // Bright cyan
 
-const kShrineBrown900 = Color(0xFF442B2D);
+const kShrineBrown900 = Color(0xFF004D40); // Deep teal/green
 
-const kShrineErrorRed = Color(0xFFC5032B);
+const kShrineErrorRed = Color(0xFFD32F2F); // Strong red (error)
 
-const kShrineSurfaceWhite = Color(0xFFFFFBFA);
-const kShrineBackgroundWhite = Colors.white;
-const kShrinePurple = Color(0xFF5D1049);
+const kShrineSurfaceWhite = Color(0xFFFFF3E0); // Warm cream
+const kShrineBackgroundWhite = Colors.white; // Default white
+const kShrinePurple = Color(0xFFFF7043);       // Vibrant orange
+
--- lib/model/products_repository.dart
+++ lib/model/products_repository.dart
@@ -8,7 +8,7 @@ class ProductsRepository {
         id: 0,
         isFeatured: true,
         name: 'Apple Watch (Silver, White Sports Band)',
-        price: 399,
+        price: 6195000, // 399 USD -> IDR
         assetName:
             'png-transparent-silver-apple-watch-with-white-sports-band-apple-watch-series-3-apple-watch-series-2-apple-watch-series-1-apple-watch-accessory-sport-apple-watch-thumbnail.png',
       ),
@@ -17,7 +17,7 @@ class ProductsRepository {
         id: 1,
         isFeatured: true,
         name: 'Calvin Klein Collection Watch',
-        price: 250,
+        price: 3875000, // 250 USD -> IDR
         assetName:
             'png-transparent-calvin-klein-collection-watch-bracelet-fashion-calvin-klein-women-watch-bracelet-holidays-women-accessories-bracelet-thumbnail.png',
       ),
@@ -26,7 +26,7 @@ class ProductsRepository {
         id: 2,
         isFeatured: false,
         name: 'Apple Watch Series 3',
-        price: 329,
+        price: 5095000, // 329 USD -> IDR
         assetName:
             'png-transparent-apple-watch-series-3-apple-watch-series-2-smartwatch-watch-electronics-watch-accessory-accessories-thumbnail.png',
       ),
@@ -35,7 +35,7 @@ class ProductsRepository {
         id: 3,
         isFeatured: true,
         name: 'Nike Apple Watch Series 3',
-        price: 349,
+        price: 5410000, // 349 USD -> IDR
         assetName:
             'png-transparent-nike-apple-watch-series-3-apple-watch-series-2-nike-watch-accessory-apple-watch-smartwatch-thumbnail.png',
       ),
@@ -44,7 +44,7 @@ class ProductsRepository {
         id: 4,
         isFeatured: false,
         name: 'Apple Watch Series 1–3 (Sport Edition)',
-        price: 289,
+        price: 4479500, // 289 USD -> IDR
         assetName:
             'png-transparent-apple-watch-series-3-apple-watch-series-2-apple-watch-series-1-smart-house-electronics-sport-apple-watch-thumbnail.png',
       ),
@@ -53,7 +53,7 @@ class ProductsRepository {
         id: 5,
         isFeatured: true,
         name: 'Michael Kors Watch',
-        price: 420,
+        price: 6510000, // 420 USD -> IDR
         assetName:
             'png-transparent-michael-kors-watch-fashion-handbag-woman-luxury-watches-watch-accessory-accessories-diamond-thumbnail.png',
       ),
@@ -62,7 +62,7 @@ class ProductsRepository {
         id: 6,
         isFeatured: false,
         name: 'Armani Chronograph Watch',
-        price: 370,
+        price: 5735000, // 370 USD -> IDR
         assetName:
             'png-transparent-watch-strap-chronograph-armani-watch-brown-watch-accessory-bracelet-thumbnail.png',
       ),
@@ -71,7 +71,7 @@ class ProductsRepository {
         id: 7,
         isFeatured: true,
         name: 'Tissot Automatic Watch',
-        price: 450,
+        price: 6975000, // 450 USD -> IDR
         assetName:
             'png-transparent-tissot-automatic-watch-luxury-goods-jewellery-tissot-mechanical-watches-electronics-watch-accessory-mens-thumbnail.png',
       ),
@@ -80,7 +80,7 @@ class ProductsRepository {
         id: 8,
         isFeatured: false,
         name: 'Apple Watch (White Pebble Edition)',
-        price: 310,
+        price: 4805000, // 310 USD -> IDR
         assetName:
             'png-transparent-apple-watch-series-2-apple-watch-series-3-pebble-apple-white-smart-watch-white-watch-accessory-computer-thumbnail.png',
       ),
@@ -89,7 +89,7 @@ class ProductsRepository {
         id: 9,
         isFeatured: true,
         name: 'Casio Edifice G-Shock',
-        price: 199,
+        price: 3085000, // 199 USD -> IDR
         assetName:
             'png-transparent-watch-casio-edifice-g-shock-water-resistant-mark-casio-men-s-waterproof-watches-electronics-watch-accessory-leather-thumbnail.png',
       ),
@@ -98,7 +98,7 @@ class ProductsRepository {
         id: 10,
         isFeatured: false,
         name: 'Apple Watch (Green Edition)',
-        price: 320,
+        price: 4960000, // 320 USD -> IDR
         assetName:
             'png-transparent-apple-watch-series-3-apple-watch-series-2-apple-watch-series-1-green-cong-electronics-watch-accessory-apple-watch-thumbnail.png',
       ),
@@ -107,7 +107,7 @@ class ProductsRepository {
         id: 11,
         isFeatured: true,
         name: 'Apple Smartwatch',
-        price: 299,
+        price: 4634500, // 299 USD -> IDR
         assetName:
             'png-transparent-apple-watch-smartwatch-wearable-technology-apple-products-electronics-gadget-company-thumbnail.png',
       ),
@@ -116,7 +116,7 @@ class ProductsRepository {
         id: 12,
         isFeatured: false,
         name: 'Casio F-91W',
-        price: 20,
+        price: 310000, // 20 USD -> IDR
         assetName:
             'png-transparent-casio-f-91w-watch-casio-la670wega-amazon-com-watch-watch-accessory-accessories-steel-thumbnail.png',
       ),
@@ -125,7 +125,7 @@ class ProductsRepository {
         id: 13,
         isFeatured: true,
         name: 'Rolex Submariner (Gold)',
-        price: 12000,
+        price: 186000000, // 12000 USD -> IDR
         assetName:
             'png-transparent-rolex-analog-watch-at-10-10-rolex-submariner-counterfeit-watch-rolex-gmt-master-ii-gold-rolex-watch-watches-gold-coin-watch-accessory-diamond-thumbnail.png',
       ),
@@ -134,7 +134,7 @@ class ProductsRepository {
         id: 14,
         isFeatured: true,
         name: 'Apple Watch (Black Stainless Steel)',
-        price: 450,
+        price: 6975000, // 450 USD -> IDR
         assetName:
             'png-transparent-apple-watch-series-2-apple-watch-series-1-smartwatch-stainless-steel-black-smart-watch-black-hair-black-white-accessories-thumbnail.png',
       ),
@@ -143,7 +143,7 @@ class ProductsRepository {
         id: 15,
         isFeatured: false,
         name: 'Montblanc Automatic Watch',
-        price: 600,
+        price: 9300000, // 600 USD -> IDR
         assetName:
             'png-transparent-montblanc-automatic-watch-watch-strap-montblanc-watch-hand-silver-rim-m-exemplar-watch-accessory-hand-leather-thumbnail.png',
       ),
@@ -152,7 +152,7 @@ class ProductsRepository {
         id: 16,
         isFeatured: true,
         name: 'Rolex Submariner (Black)',
-        price: 9800,
+        price: 151900000, // 9800 USD -> IDR
         assetName:
             'png-transparent-round-black-rolex-analog-watch-with-bracelet-rolex-submariner-rolex-daytona-rolex-datejust-rolex-gmt-master-ii-black-male-watch-rolex-watch-black-hair-watch-accessory-black-white-thumbnail.png',
       ),
@@ -161,7 +161,7 @@ class ProductsRepository {
         id: 17,
         isFeatured: true,
         name: 'Rolex Automatic Gold Watch',
-        price: 15000,
+        price: 232500000, // 15000 USD -> IDR
         assetName:
             'png-transparent-automatic-watch-rolex-watch-strap-gold-watches-rolex-watches-female-form-watch-accessory-gold-coin-time-thumbnail.png',
       ),
@@ -170,7 +170,7 @@ class ProductsRepository {
         id: 18,
         isFeatured: false,
         name: 'IWC Chronograph Watch',
-        price: 7200,
+        price: 111600000, // 7200 USD -> IDR
         assetName:
             'png-transparent-analog-watch-international-watch-company-calatrava-chronograph-cheetah-men-s-watches-animals-bracelet-watch-accessory-thumbnail.png',
       ),
@@ -179,7 +179,7 @@ class ProductsRepository {
         id: 19,
         isFeatured: true,
         name: 'Gold Quartz Watch',
-        price: 400,
+        price: 6200000, // 400 USD -> IDR
         assetName:
             'png-transparent-watch-quartz-clock-gold-watch-watch-accessory-accessories-diamond-thumbnail.png',
       ),
@@ -188,7 +188,7 @@ class ProductsRepository {
         id: 20,
         isFeatured: false,
         name: 'Classic Pocket Watch',
-        price: 180,
+        price: 2790000, // 180 USD -> IDR
         assetName:
             'png-transparent-pocket-watch-clock-men-s-watch-watch-accessory-poster-accessories-thumbnail.png',
       ),
@@ -197,7 +197,7 @@ class ProductsRepository {
         id: 21,
         isFeatured: false,
         name: 'Apple Watch Series 3 (with MacBook Pro)',
-        price: 360,
+        price: 5580000, // 360 USD -> IDR
         assetName:
             'png-transparent-apple-watch-series-3-apple-watch-series-2-macbook-pro-apple-watch-series-3-thumbnail.png',
       ),
@@ -206,7 +206,7 @@ class ProductsRepository {
         id: 22,
         isFeatured: true,
         name: 'Smartwatch (BBCode Generic)',
-        price: 150,
+        price: 2325000, // 150 USD -> IDR
         assetName:
             'png-transparent-bbcode-smart-watch-miscellaneous-electronics-gadget-thumbnail.png',
       ),
@@ -215,7 +215,7 @@ class ProductsRepository {
         id: 23,
         isFeatured: false,
         name: 'Skeleton Automatic Watch',
-        price: 510,
+        price: 7905000, // 510 USD -> IDR
         assetName:
             'png-transparent-skeleton-watch-automatic-watch-international-watch-company-refined-watches-watch-accessory-accessories-diamond-thumbnail.png',
       ),
@@ -224,7 +224,7 @@ class ProductsRepository {
         id: 24,
         isFeatured: true,
         name: 'Vacheron Constantin Gold Watch',
-        price: 18000,
+        price: 279000000, // 18000 USD -> IDR
         assetName:
             'png-transparent-vacheron-constantin-gold-watch-movement-strap-vacheron-constantin-watches-black-watches-mechanical-male-watch-black-hair-watch-accessory-black-white-thumbnail.png',
       ),
@@ -233,7 +233,7 @@ class ProductsRepository {
         id: 25,
         isFeatured: false,
         name: 'Designer Women’s Fashion Watch',
-        price: 210,
+        price: 3255000, // 210 USD -> IDR
         assetName:
             'png-transparent-watch-clock-fashion-accessory-designer-women-s-watch-holidays-women-accessories-indian-nude-women-thumbnail.png',
       ),
@@ -242,7 +242,7 @@ class ProductsRepository {
         id: 26,
         isFeatured: true,
         name: 'Fossil Chronograph Watch',
-        price: 190,
+        price: 2945000, // 190 USD -> IDR
         assetName:
             'png-transparent-amazon-com-fossil-group-chronograph-watch-jewellery-omega-watch-watch-accessory-metal-watch-thumbnail.png',
       ),
@@ -251,7 +251,7 @@ class ProductsRepository {
         id: 27,
         isFeatured: true,
         name: 'Tissot Chronograph Watch',
-        price: 390,
+        price: 6045000, // 390 USD -> IDR
         assetName:
             'png-transparent-watch-chronograph-tissot-jewellery-strap-citizen-men-s-watch-watch-accessory-accessories-apple-watch-thumbnail.png',
       ),
@@ -260,7 +260,7 @@ class ProductsRepository {
         id: 28,
         isFeatured: false,
         name: 'Titan Company Ladies Watch',
-        price: 140,
+        price: 2170000, // 140 USD -> IDR
         assetName:
             'png-transparent-watch-strap-titan-company-metal-gold-ladies-watch-thumbnail.png',
       ),
@@ -269,7 +269,7 @@ class ProductsRepository {
         id: 29,
         isFeatured: false,
         name: 'Daniel Wellington Watch',
-        price: 180,
+        price: 2790000, // 180 USD -> IDR
         assetName:
             'png-transparent-watch-strap-daniel-wellington-watch-watch-accessory-accessories-apple-watch-thumbnail.png',
       ),
@@ -278,7 +278,7 @@ class ProductsRepository {
         id: 30,
         isFeatured: true,
         name: 'Omega Speedmaster / Tag Heuer Chronograph',
-        price: 7000,
+        price: 108500000, // 7000 USD -> IDR
         assetName:
             'png-transparent-omega-speedmaster-tag-heuer-watch-chronograph-rolex-tag-heuer-watch-silver-wristwatch-male-watch-sale-tag-watch-accessory-mens-thumbnail.png',
       ),
@@ -287,7 +287,7 @@ class ProductsRepository {
         id: 31,
         isFeatured: false,
         name: 'Silver Strap Women’s Watch',
-        price: 160,
+        price: 2480000, // 160 USD -> IDR
         assetName:
             'png-transparent-watch-strap-silver-women-watch-thumbnail.png',
       ),
@@ -296,7 +296,7 @@ class ProductsRepository {
         id: 32,
         isFeatured: true,
         name: 'Tudor Oyster Watch',
-        price: 3800,
+        price: 58900000, // 3800 USD -> IDR
         assetName:
             'png-transparent-tudor-watches-rolex-oyster-wrist-watch-watch-accessory-bracelet-accessories-thumbnail.png',
       ),
@@ -305,7 +305,7 @@ class ProductsRepository {
         id: 33,
         isFeatured: false,
         name: 'Retail Product Watch (Generic)',
-        price: 99,
+        price: 1535000, // 99 USD -> IDR
         assetName:
             'png-transparent-watch-clock-watch-accessory-retail-product-thumbnail.png',
       ),
@@ -314,7 +314,7 @@ class ProductsRepository {
         id: 34,
         isFeatured: true,
         name: 'Wristwatch Icon (Brown Strap)',
-        price: 120,
+        price: 1860000, // 120 USD -> IDR
         assetName:
             'png-transparent-watch-icon-wristwatch-brown-watch-accessory-candle-thumbnail.png',
       ),
